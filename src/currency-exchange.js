d37d6412ceb3dd1ebbd66afaf0b3c92e53cffa46
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-03 13:36:32 -0700

    make all conversions work. math is really hard

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -7,11 +7,12 @@ import { CurrencyExchange } from './../src/currency-exchange-service.js';
 $(document).ready(function() {
 
   $('#exchange').click(function() {
-    const usd = parseInt($('#us-dollars').val());
+    const amount = parseInt($('#amount').val());
     const currency = $('#currency').val();
-    $('#us-dollars').val("");
+    const baseCurrency = $('#base-currency').val();
+    $('#amount').val("");
     
-    if (!isNaN(usd)) {
+    if (!isNaN(amount)) {
       (async () => {
         let currencyExchange = new CurrencyExchange();
         const response = await currencyExchange.getExchange();
@@ -21,10 +22,10 @@ $(document).ready(function() {
       $('.showErrors').text("Please enter a valid amount");
       }
     function getElements(response) {
-      console.log(typeof response.conversion_rates[currency])
-
+      let convertToDollars = amount / response.conversion_rates[baseCurrency];
+      convertToDollars * response.conversion_rates[currency]
       if (response && response.conversion_rates[currency] !== undefined) {
-        $('.showExchange').text(`The amount of ${usd} is equal to ${response.conversion_rates[currency] * usd} in ${currency}`);
+        $('.showExchange').text(`The amount of ${amount} in ${baseCurrency}is equal to ${convertToDollars * response.conversion_rates[currency]} in ${currency}`);
       } else {
         $('.showErrors').text("There was an error processing your request.");
       }
