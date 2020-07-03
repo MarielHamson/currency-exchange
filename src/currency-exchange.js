43977fd3fe9494d4eb33b97c4945a19f476abc9c
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-03 11:36:35 -0700

    update error handling for incorrect input

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -7,21 +7,27 @@ import { CurrencyExchange } from './../src/currency-exchange-service.js';
 $(document).ready(function() {
 
   $('#exchange').click(function() {
-    const usd = $('#us-dollars').val();
+    const usd = parseInt($('#us-dollars').val());
     const currency = $('#currency').val();
-    $('#us-dollars').val("");
+    // $('#us-dollars').val("");
     
-    (async () => {
-      let currencyExchange = new CurrencyExchange();
-      const response = await currencyExchange.getExchange();
-      getElements(response);
-    })();
+    console.log((usd.isNaN))
+    if (!isNaN(usd)) {
+      (async () => {
+        let currencyExchange = new CurrencyExchange();
+        const response = await currencyExchange.getExchange();
+        getElements(response);
+        $('.showErrors').hide();
+      })();
+    } else {
+      $('.showErrors').text("Please enter a valid amount");
+      }
 
     function getElements(response) {
       if (response) {
         $('.showExchange').text(`The amount of ${usd} is equal to ${response.conversion_rates[currency] * usd} in ${currency}`);
       } else {
-        $('.showError').text("There was an error processing your request.");
+        $('.showErrors').text("There was an error processing your request.");
       }
     }
   });
