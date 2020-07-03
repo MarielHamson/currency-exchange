1ae702b803fb149de3aa0c05e48f67207676daf7
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-03 16:19:05 -0700

    add additional error handling and fix indentation

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -1,6 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/styles.css'
+import '../src/styles.css';
 import $ from 'jquery';
 import { CurrencyExchange } from './../src/currency-exchange-service.js';
 
@@ -10,13 +10,13 @@ $(document).ready(function() {
     const amount = parseInt($('#amount').val());
     const currency = $('#currency').val();
     const baseCurrency = $('#base-currency').val();
-    $('#amount').val("");
-    
+    $('#amount').val("");    
     if (!isNaN(amount)) {
       (async () => {
         let currencyExchange = new CurrencyExchange();
         const response = await currencyExchange.getExchange();
         getElements(response);
+        $('.showErrors').hide();
       })();
     } else {
       $('.showErrors').text("Please enter a valid amount");
@@ -25,9 +25,8 @@ $(document).ready(function() {
       let convertToDollars = Math.round(amount / response.conversion_rates[baseCurrency], 2);
       if (response && response.conversion_rates[currency] !== undefined) {
         $('.showExchange').text(`${amount} in ${baseCurrency} is equal to about ${Number.parseFloat(convertToDollars * response.conversion_rates[currency]).toFixed(2)} in ${currency}`);
-      } else {
-        $('.showErrors').text("There was an error processing your request.");
-      }
+      } else if (response.status !== 200 || response.conversion_rates[currency == undefined]) {
+        $('.showErrors').text("There was an error processing your request. Error code: " + response.status)}
     }
   });
 });
