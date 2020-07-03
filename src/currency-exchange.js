0691894afefb455937f57d94b8178c12cd564bbf
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-03 15:28:41 -0700

    add bg img and styling

--- src/currency-exchange.js
+++ src/currency-exchange.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/styles.css'
 import $ from 'jquery';
 import { CurrencyExchange } from './../src/currency-exchange-service.js';
 
@@ -19,11 +20,11 @@ $(document).ready(function() {
       })();
     } else {
       $('.showErrors').text("Please enter a valid amount");
-      }
+    }
     function getElements(response) {
       let convertToDollars = Math.round(amount / response.conversion_rates[baseCurrency], 2);
       if (response && response.conversion_rates[currency] !== undefined) {
-        $('.showExchange').text(`The amount of ${amount} in ${baseCurrency} is equal to about ${Number.parseFloat(convertToDollars * response.conversion_rates[currency]).toFixed(2)} in ${currency}`);
+        $('.showExchange').text(`${amount} in ${baseCurrency} is equal to about ${Number.parseFloat(convertToDollars * response.conversion_rates[currency]).toFixed(2)} in ${currency}`);
       } else {
         $('.showErrors').text("There was an error processing your request.");
       }
